084db24c37a2bb26d72d1f6e5f715b07f9fd5453
baktistr <bakti.satria.a@gmail.com>
2025-10-07 19:19:33 -0400

    Clean install: Astro + Starlight with Rapide theme

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://baktistr.github.io',
+	integrations: [
+		starlight({
+			title: 'My Blog',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/baktistr' }
+			],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			plugins: [starlightThemeRapide()],
+		}),
+	],
+});
